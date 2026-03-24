dfba2073a9d2258cd4cb077339f42724335cada6
codeYx <2584912193@qq.com>
2026-03-24 14:53:02 +0800

    第一次提交

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import * as Sentry from '@sentry/vue'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: 'https://3147bf2f9671f799dda27fabe4dd1b90@o4511097411796992.ingest.us.sentry.io/4511097883721728',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+})
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>dada</main>
+</template>
+
+<script setup lang="ts"></script>
