4f065506ea3e41b840c216d6b72d2e51d7de6aca
codeYx <2584912193@qq.com>
2026-03-24 15:19:36 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as Sentry from '@sentry/vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(ElementPlus)
 
 Sentry.init({
   app,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <template>
-  <main>dada</main>
+  <main>
+    <el-input v-model="input1" style="width: 240px" placeholder="请输入第一个数字" />
+    <el-input v-model="input2" style="width: 240px" placeholder="请输入第二个数字" />
+    <el-button type="primary" @click="calculate">结果</el-button>
+    <br />
+    我的计算结果{{ result }}
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const input1 = ref(0)
+const input2 = ref(0)
+const result = ref(0)
+const calculate = () => {
+  result.value = input1.value / input2.value
+}
+</script>
